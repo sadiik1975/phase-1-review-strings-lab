de2367986ff594ff51081478e6257d1e59434073
Sadeek Been <sadiikbeen@gmail.com>
2024-07-27 02:46:50 -0400

    updating file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'User Name';
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
